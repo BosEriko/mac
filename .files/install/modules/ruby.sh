00c7a77650d3f1ace56809af7364f85eac009bb8
BosEriko <bos@eriko.ph>
2026-01-20 10:10:44 +0800

    fix ruby install script

--- .files/install/modules/ruby.sh
+++ .files/install/modules/ruby.sh
@@ -1,25 +1,10 @@
 #!/bin/bash
 
-# Remove Ruby
-sudo apt-get purge -y ruby ruby-dev
-sudo apt-get autoremove -y
-
-# Install Prerequisites
-sudo apt install -y libffi-dev
-sudo apt install -y libyaml-dev
-sudo apt install -y rbenv
-
-# Ruby build (https://github.com/rbenv/ruby-build)
-mkdir -p "$(rbenv root)"/plugins
-git clone https://github.com/rbenv/ruby-build.git "$(rbenv root)"/plugins/ruby-build
-
-# Update ruby build list (https://github.com/rbenv/ruby-build)
-git -C "$(rbenv root)"/plugins/ruby-build pull
+# Install rbenv
+brew install rbenv
 
 # Source rbenv
-export PATH="$HOME/.rbenv/bin:$PATH"
-eval "$(rbenv init -)"
-export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"
+export PATH="$HOME/.rbenv/bin:$PATH" && eval "$(rbenv init -)"
 
 # Initialize rbenv and set global ruby version (https://github.com/rbenv/rbenv)
 RUBY_VERSION=3.3.3
