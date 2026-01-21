235f6f3cd4996b11e33cb348c4b757c91f667c0b
BosEriko <bos@eriko.ph>
2026-01-21 12:43:58 +0800

    add tmux install script

---
+++ .files/install/modules/tmux.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Install tpm (https://github.com/tmux-plugins/tpm)
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
---
+++ .files/tmux/plugins.tmux.sh
@@ -0,0 +1,9 @@
+
+# ================================================================================ [Plugins] ===== #
+
+# Bottom Bar
+set -g @plugin "arcticicestudio/nord-tmux"
+
+# Initialize the plugin manager
+run '~/.tmux/plugins/tpm/tpm'
+
