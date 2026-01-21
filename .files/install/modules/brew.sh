ff610fc87dfa654df24dab0bcaa500aa51c976b1
BosEriko <bos@eriko.ph>
2026-01-21 12:33:13 +0800

    add tmux

--- .files/install/modules/brew.sh
+++ .files/install/modules/brew.sh
@@ -6,6 +6,7 @@ packages=(
   jq            # Command-line JSON processor
   zsh           # Z shell (macOS ships with zsh, but this installs the latest version)
   antigen       # Zsh plugin manager
+  tmux		# Terminal multiplexer
 )
 
 # Update Homebrew
---
+++ .files/tmux/theme.tmux.sh
@@ -0,0 +1,6 @@
+
+# ================================================================================== [Theme] ===== #
+
+set -wg mode-style bg='#87D787',fg='#272727'
+set -g pane-active-border-style fg='#272727'
+set -g pane-border-style fg='#272727'
