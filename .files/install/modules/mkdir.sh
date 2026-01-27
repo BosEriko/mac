77a7092e846facbb2a38f370249ddf5c0671ccf7
BosEriko <bos@eriko.ph>
2026-01-27 17:35:24 +0800

    remove anime script (zsh)

--- .files/install/modules/mkdir.sh
+++ .files/install/modules/mkdir.sh
@@ -2,6 +2,3 @@
 
 echo "Make the directories for Personal/Work/Configuration"
 mkdir -p ~/Documents/Codes/Work ~/Documents/Codes/Personal ~/Documents/Codes/Configuration
-
-echo "Make the directory for your Anime"
-mkdir -p ~/Movies/Anime
