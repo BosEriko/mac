cd4c5bc98b992dc4db27096403542f09685ee77f
BosEriko <bos@eriko.ph>
2026-01-27 11:05:22 +0800

    add the configuration folder creation on mkdir

--- .files/install/modules/mkdir.sh
+++ .files/install/modules/mkdir.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-echo "Make the directories for Personal/Work"
-mkdir -p ~/Documents/Codes/Work ~/Documents/Codes/Personal
-
-echo "Make the directory for your Films"
-mkdir -p ~/Movies/Films
+echo "Make the directories for Personal/Work/Configuration"
+mkdir -p ~/Documents/Codes/Work ~/Documents/Codes/Personal ~/Documents/Codes/Configuration
 
 echo "Make the directory for your Anime"
 mkdir -p ~/Movies/Anime
+
+echo "Make the directory for your Films"
+mkdir -p ~/Movies/Films
 
 echo "Make the directory for YouTube videos"
 mkdir -p ~/Movies/YouTube
