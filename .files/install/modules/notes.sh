cfa70e2797b1401e7b5153a23bb8b9b21d5d7031
BosEriko <bos@eriko.ph>
2026-01-30 10:32:51 +0800

    remove .zsh_history and move it to obsidian

--- .files/install/modules/notes.sh
+++ .files/install/modules/notes.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-mkdir -p ~/Documents
 brew install --cask obsidian
+mkdir -p ~/Documents
 git clone https://github.com/BosEriko/obsidian.git ~/Documents/Notes
+rm ~/.zsh_history && ln -sf ~/Documents/Notes/.zsh_history ~/.zsh_history
