9e5e4c62d3733eeb0773edfdb30f308b6bf9dcb9
BosEriko <bos@eriko.ph>
2026-02-20 19:34:16 +0800

    Feature: Disable removing of tmux bar on file explorer

--- .files/zsh/yazi/mac.z.sh
+++ .files/zsh/yazi/mac.z.sh
@@ -3,10 +3,8 @@
 
 function tree() {
   local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
-  [ -n "$TMUX" ] && tmux set-option -g status off
   command yazi "$@" --cwd-file="$tmp"
-  [ -n "$TMUX" ] && tmux set-option -g status on
-  IFS= read -r -d '' cwd < "$tmp"
+  IFS= read -r -d '' cwd <"$tmp"
   [ "$cwd" != "$PWD" ] && [ -d "$cwd" ] && builtin cd -- "$cwd"
   rm -f -- "$tmp"
 }
